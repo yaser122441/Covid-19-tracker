dcac831b5ef26dc4a956c39b6d0ca93153b57d81
yaser alhamad <yaser.alhamadwork@gmail.com>
2022-09-15 18:56:30 +0300

    Updates

---
+++ precache-manifest.2d57f2d179c63b1cb4df468d52d11be6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f847c99c412e0dc1d8832c2d1ec91155",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "01ff27e02219205f7b9b",
+    "url": "/covid-19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "25f65089b6b6f7528dca",
+    "url": "/covid-19-tracker/static/css/main.02729728.chunk.css"
+  },
+  {
+    "revision": "01ff27e02219205f7b9b",
+    "url": "/covid-19-tracker/static/js/2.7c300910.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-19-tracker/static/js/2.7c300910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f65089b6b6f7528dca",
+    "url": "/covid-19-tracker/static/js/main.b551585a.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
